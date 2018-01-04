3df697eb6ed09a7f8ea7d54076444058a0f4ff05
Ézio <ezioadsr@fagoc.br>
2018-01-04 11:40:50 -0200

    Funções de comparação de datas (Greater than e Less than)

--- support/date/index.js
+++ support/date/index.js
@@ -7,3 +7,33 @@ import moment from 'moment'
 export const today = (format = 'YYYY-MM-DD') => {
   return moment(new Date()).startOf('day').format(format)
 }
+
+/**
+ * @param {string} value
+ * @param {string} comparator
+ * @param {boolean} equal
+ * @returns {null|boolean}
+ */
+export const isDateGreaterThan = (value, comparator, equal = false) => {
+  let date1 = moment(value)
+  if (!date1.isValid()) {
+    return null
+  }
+
+  let date2 = moment(comparator)
+  if (!date2.isValid()) {
+    return null
+  }
+
+  return equal ? date1 >= date2 : date1 > date2
+}
+
+/**
+ * @param {string} value
+ * @param {string} comparator
+ * @param {boolean} equal
+ * @returns {null|boolean}
+ */
+export const isDateLessThan = (value, comparator, equal = false) => {
+  return isDateGreaterThan(comparator, value, equal)
+}
