ea1089653220f00497db5478a0d502aab0f89804
William Correa <wilcorrea@gmail.com>
2018-01-23 21:54:49 -0200

    Funções para processamentos dos fields dos forms agrupadas todas no mesmo lugar

---
+++ support/model/form.js
@@ -0,0 +1,49 @@
+/**
+ * @param scope
+ * @returns {function(*): (boolean|*)}
+ */
+export const filter = (scope) => {
+  return (item) => item.scopes.includes(scope)
+}
+
+/**
+ * @param readonly
+ * @param component
+ * @returns {function(*): ({} & any & {disabled: boolean, field, component: string})}
+ */
+export const map = (readonly, component) => {
+  return (item) => Object.assign({}, item.form, {
+    disabled: readonly ? true : item.form.disabled,
+    field: item.field,
+    component: item.form.component ? (component + '-' + item.form.component) : ''
+  })
+}
+
+/**
+ *
+ * @param a
+ * @param b
+ * @returns {number}
+ */
+export const sort = (a, b) => {
+  if (!a.order || !b.order) {
+    return 0
+  }
+  if (a.order < b.order) {
+    return -1
+  }
+  if (a.order > b.order) {
+    return 1
+  }
+  return 0
+}
+
+/**
+ * @param accumulate
+ * @param item
+ * @return {*}
+ */
+export const reduce = (accumulate, item) => {
+  accumulate[item.field] = item
+  return accumulate
+}
